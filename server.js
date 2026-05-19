10b5b3b920a27ad6e8b5361d18c79fcd3d4b082c
sokol <sokol@example.com>
2026-05-19 02:20:45 +0200

    fixed favicon

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-/* =========================
-   STATIC WEBSITE
-========================= */
-
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 /* =========================
    USERS FILE
